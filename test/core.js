9618e32be8413b490c2431fe354c0fb3f814bd49
mdaniel <mdaniel@192.168.2.1>
2010-02-22 22:05:13 +0100

    Test test...

--- test/core.js
+++ test/core.js
@@ -1,8 +1,4 @@
 (function(){
-   
-    test('Fail jQuery UI Widget Controller', function(){
-		ok(false, "Untested code is broken code!");       
-    });
 	
 	test('Pass jQuery UI Widget Controller', function(){
 		ok(true, "Untested code should be broken code!");       
