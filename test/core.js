ad78ff58f04ca91ed031c68d97674b773b5f04f4
mdaniel <mdaniel@192.168.2.1>
2010-02-22 23:10:02 +0100

    More tests.

--- test/core.js
+++ test/core.js
@@ -3,5 +3,28 @@
 	test('Pass jQuery UI Widget Controller', function(){
 		ok(true, "Untested code should be broken code!");       
     });
+	
+	
+	test('Pass jQuery UI Widget Controller', function(){
+		ok(true, "Untested code should be broken code!");       
+    });
+	
+	
+	test('Pass jQuery UI Widget Controller', function(){
+		ok(true, "Untested code should be broken code!");       
+    });
+	
+	test('Pass jQuery UI Widget Controller', function(){
+		ok(true, "Untested code should be broken code!");
+		ok(true, "Untested code should be broken code!");
+		ok(true, "Untested code should be broken code!");
+		ok(true, "Untested code should be broken code!");
+		ok(true, "Untested code should be broken code!");
+		ok(true, "Untested code should be broken code!");
+		ok(true, "Untested code should be broken code!");
+		ok(true, "Untested code should be broken code!");
+		ok(true, "Untested code should be broken code!");
+		ok(true, "Untested code should be broken code!");       
+    });
     
 })();
