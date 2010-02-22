2527a3bf59fe5efdeb183a34988e47e1db6cc425
mdaniel <mdaniel@192.168.2.1>
2010-02-22 22:01:38 +0100

    Unit test test?

--- test/core.js
+++ test/core.js
@@ -1,7 +1,11 @@
 (function(){
    
-    test('jQuery UI Widget Controller', function(){
+    test('Fail jQuery UI Widget Controller', function(){
 		ok(false, "Untested code is broken code!");       
-    })
+    });
+	
+	test('Pass jQuery UI Widget Controller', function(){
+		ok(true, "Untested code should be broken code!");       
+    });
     
 })();
