e79eae87570bfe7defdfcd49e9338067341ce0c9
mdaniel <mdaniel@192.168.2.1>
2010-02-28 20:29:11 +0100

    Jekyll Test

---
+++ js/bootstrap.js
@@ -0,0 +1,27 @@
+/**
+ * @author mdaniel
+ */
+(function(){
+    var Constants = {
+        SWITCHER_SEL: "#wt-switcher"
+    };
+    
+    // Setup console case not defined
+    if (typeof console === "undefined") {
+        this.console = {};
+        this.console.log = function(){
+        };
+        this.console.info = function(){
+        };
+        this.console.debug = function(){
+        };
+        this.console.warn = function(){
+        };
+        this.console.error = function(){
+        };
+    }
+    
+    // ThemeRoller demo
+    $(Constants.SWITCHER_SEL).themeswitcher();
+    
+})();
