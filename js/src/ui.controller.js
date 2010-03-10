b319791ef4a6c8731df38934b9ee9a122894f7a4
Mickael DANIEL <mdaniel@MacBook-Pro.home>
2010-03-10 19:35:02 +0100

    Auto commit

--- js/src/ui.controller.js
+++ js/src/ui.controller.js
@@ -50,7 +50,7 @@
         return path;
     };
 	
-    var Class, SandBox, BaseController;
+    var SandBox, BaseController;
 	
 	/**
      * Based on DUI's one.  <br />
@@ -64,7 +64,9 @@
      * 
      */
     var ns = function(){
-        if (arguments.length == 0) throw new Error('ns should probably have some arguments passed to it.');
+        if (arguments.length === 0){
+			throw new Error('ns should probably have some arguments passed to it.');
+		}
         
         var arg = arguments[0];
         var levels = null;
@@ -289,7 +291,7 @@
 	    })(options.url);
 		
 	    
-	    if (!options || requireCache[options.url] !== null) {
+	    if (!options || requireCache[options.url] === true) {
 	        // File is already loaded, immediately execute the callback
 	        if ($.isFunction(callback)) {
 	            callback();
@@ -366,75 +368,6 @@
 	};
 	
 	require.namespace = {};
-	
-	
-    /**
-     * Base Class.
-     * 
-     * Based on jresig's implementaton (http://ejohn.org/blog/simple-javascript-inheritance/)
-     * 
-     * All subsequent controllers are inherited from BaseController which extend this Class.
-     * 
-     * @class Class
-     * 
-     */
-    (function(){
-        var 
-			initializing = false, 
-			fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-        
-        // The base Class implementation (does nothing)
-        Class = function(){};
-        
-        // Create a new Class that inherits from this class
-        Class.extend = function(prop){
-            var _super = this.prototype;
-            
-            // Instantiate a base class (but only create the instance,
-            // don't run the init constructor)
-            initializing = true;
-            var prototype = new this();
-            initializing = false;
-            
-            // Copy the properties over onto the new prototype
-            for (var name in prop) {
-                // Check if we're overwriting an existing function
-                prototype[name] = typeof prop[name] == "function" && typeof _super[name] == "function" && fnTest.test(prop[name]) ? (function(name, fn){
-                    return function(){
-                        var tmp = this._super;
-                        
-                        // Add a new ._super() method that is the same method
-                        // but on the super-class
-                        this._super = _super[name];
-                        
-                        // The method only need to be bound temporarily, so we
-                        // remove it when we're done executing
-                        var ret = fn.apply(this, arguments);
-                        this._super = tmp;
-                        
-                        return ret;
-                    };
-                })(name, prop[name]) : prop[name];
-            }
-            
-            // The dummy class constructor
-            function Class(){
-                // All construction is actually done in the init method
-                if (!initializing && this.init) this.init.apply(this, arguments);
-            }
-            
-            // Populate our constructed prototype object
-            Class.prototype = prototype;
-            
-            // Enforce the constructor to be what we expect
-            Class.constructor = Class;
-            
-            // And make this class extendable
-            Class.extend = arguments.callee;
-            
-            return Class;
-        };
-    })();
     
     /**
      * SandBox Singleton available in any controllers instance. <br />
@@ -479,7 +412,7 @@
      * @class BaseController
      * @extends Class
      */
-    BaseController = Class.extend({
+    BaseController = $.Class.extend({
 		/**
 		 * Init method. Called each time a controller is destroyed 
 		 * (usually removed from the dom).
